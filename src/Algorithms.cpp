05dd81ad89386df1c9771238abdeb1957053101d
Eshal <eshaladnannn1@gmail.com>
2025-11-15 02:48:42 +0500

    Fix ordering, remove void casts, resolve merge conflicts for insertion sort

--- src/Algorithms.cpp
+++ src/Algorithms.cpp
@@ -2,45 +2,7 @@
 #include <algorithm>
 #include <vector>
 
-// -------------------- Insertion Sort --------------------
-bool InsertionSort::step(std::vector<int>& data)
-{
-    if (done || data.empty()) return false;
 
-    if (i < static_cast<int>(data.size()))
-    {
-        if (j == i - 1)
-            key = data[i];
-
-        if (j >= 0 && data[j] > key)
-        {
-            data[j + 1] = data[j];
-            --j;
-        }
-        else
-        {
-            data[j + 1] = key;
-            ++i;
-            j = i - 1;
-        }
-    }
-    else
-    {
-        done = true;
-        return false;
-    }
-
-    return true;
-}
-
-void InsertionSort::reset(std::vector<int>& data)
-{
-    (void)data;
-    done = false;
-    i = 1;
-    j = 0;
-    key = 0;
-}
 
 // -------------------- Bubble Sort --------------------
 bool BubbleSort::step(std::vector<int>& data)
@@ -89,7 +51,45 @@ void SelectionSort::reset(std::vector<int>& data)
     (void)data;
     // reset internal state if implemented
 }
+// -------------------- Insertion Sort --------------------
+bool InsertionSort::step(std::vector<int>& data)
+{
+    if (done || data.empty()) return false;
 
+    if (i < static_cast<int>(data.size()))
+    {
+        if (j == i - 1)
+            key = data[i];
+
+        if (j >= 0 && data[j] > key)
+        {
+            data[j + 1] = data[j];
+            --j;
+        }
+        else
+        {
+            data[j + 1] = key;
+            ++i;
+            j = i - 1;
+        }
+    }
+    else
+    {
+        done = true;
+        return false;
+    }
+
+    return true;
+}
+
+void InsertionSort::reset(std::vector<int>& data)
+{
+    (void)data;
+    done = false;
+    i = 1;
+    j = 0;
+    key = 0;
+}
 // -------------------- Comb Sort (stub) --------------------
 bool CombSort::step(std::vector<int>& data)
 {
